ffa1d81f98a483c4108b38fe873e0d9c2cce18f9
fpalomerosanz <fpalomerosanz@gmail.com>
2024-05-07 12:03:37 -0400

    estructura basica

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express  from "express";
 import { options } from "./config/config.js";
 import { dbConection } from "./config/dbConection.js";
+import { __dirname } from "./utils.js";
+import { engine } from 'express-handlebars';
+import path from "path";
 
 //APP
 const app = express();
@@ -8,10 +11,18 @@ const app = express();
 //MIDDLEWARES
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use('/static', express.static(__dirname + 'public'))
 
 //SERVER
 const server = app.listen(options.server.port, ()=>{
     console.log('Servidor todavia funcionando en el puerto ochenta ochenta');
 })
+
 //BDD
 dbConection();
+
+//HANDLEBARS
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname,"/views"));
+
