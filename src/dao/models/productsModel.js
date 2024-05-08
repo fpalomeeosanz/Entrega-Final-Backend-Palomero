0164ef7c55f15a589cc0a3c55963501708dac1ad
fpalomerosanz <fpalomerosanz@gmail.com>
2024-05-08 19:11:32 -0400

    depuracion en mongoDB

--- src/dao/models/productsModel.js
+++ src/dao/models/productsModel.js
@@ -4,11 +4,11 @@ import mongoosePaginate from "mongoose-paginate-v2";
 const collection = "products";
 
 const productsSchema= new mongoose.Schema({
-    title: {type: String,required:true},
+    code: {type: String,required:true},
     price: { type: Number,required:true,min:1},
     description: { type: String, required: true },
     stock: {type: Number,required:true},
-    image: { type: String, required: true },
+    thumbnail: { type: String, required: true },
     category:{ type: String,required:true,enum: ['Hospedaje', 'Salon', 'Actividades', 'Delicias', 'Eventos']},
     owner:{
         type:mongoose.Schema.Types.ObjectId,
